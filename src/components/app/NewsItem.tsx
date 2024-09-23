a21970af66f09ef579b43bdf50a7d300cb9eac32
ixyz <m@ixyz.org>
2024-09-23 22:05:10 +0800

    首页完成

--- src/components/app/NewsItem.tsx
+++ src/components/app/NewsItem.tsx
@@ -1,4 +1,19 @@
-const NewsItem: React.FC = () => {
-  return <div>NewsItem</div>
+import './NewsItem.scss'
+import { Link } from 'react-router-dom'
+const NewsItem: React.FC<any> = (props) => {
+  const { id, hint, title, images } = props
+  return (
+    <div className="news-item">
+      <Link to={`/details/${id}`}>
+        <div className="content">
+          <h4 className="title">{title}</h4>
+          <p className="author">{hint}</p>
+        </div>
+        <div className="img-container">
+          <img src={images[0]} />
+        </div>
+      </Link>
+    </div>
+  )
 }
 export default NewsItem
--- src/components/app/SkeletonCustom.tsx
+++ src/components/app/SkeletonCustom.tsx
@@ -1,10 +1,11 @@
 import { Skeleton } from 'antd-mobile'
+import './SkeletonCustom.scss'
 const SkeletonCustom: React.FC = () => {
   return (
-    <>
+    <div className='skeleton-custom-container'>
       <Skeleton.Title animated />
       <Skeleton.Paragraph lineCount={5} animated />
-    </>
+    </div>
   )
 }
 export default SkeletonCustom
